fc4712a68981fb6830c8203806c4143aa64edbe3
Can Bin <canbin60@gmail.com>
2021-12-12 18:19:52 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Navbar from './components/navbar/Navbar'
+import Header from './components/header/Header'
+import Cards from './components/cards/Cards';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Header/>
+      <Cards/>
+    </div>
+  );
+}
+
+export default App;
